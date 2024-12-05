c6f7e0ecd8676106fc5b1f196b00b41f7be2ae1a
Kristina018 <girl.frankie@gmail.com>
2024-12-05 16:35:04 +0200

    gerai

--- task1.py
+++ task1.py
@@ -1,22 +1,32 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 # A) Duotas sąrašas x=[1,2,3,4,5,6,7,8,9]
-# Viename lange, skirtinguose grafikuose atvaizduokite 𝑥 , 𝑥 ⋅ 3,
-# 𝑥 ⋅ 𝑎, čia a - įvedamas vartotojo.
+# Viename lange, skirtinguose grafikuose atvaizduokite 𝑥^2, 𝑥⋅3, 𝑥⋅𝑎, čia a - įvedamas vartotojo.
 # Panaudokite skirtingas spalvas, linijų tipus.
 
-# %matplotlib qt
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+x_three = np.array(x) * 3
+x_zero = np.array(x) * 0
 
 def squared(x):
-    return x ** 2
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# y = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# x_squared = x * 2
-# x_3 = x * 3
-# a = int(input(f'Įveskite kažkokį skaičių: '))
-# x_input = x * a
+    return np.array(x) ** 2
+
+def input_a():
+    a = int(input(f'Įveskite kažkokį skaičių: '))
+    return np.array(x) * a # tik asys keiciasi
+
 langas, grafikas = plt.subplots(2, 2)
-grafikas.plot(x, squared(x))
+grafikas[0][0].plot(x, squared(x))
+grafikas[0][1].plot(x, input_a())
+grafikas[1][0].plot(x, x_three)
+grafikas[1][1].plot(x, x_zero)
+
+
 plt.show()
 
+# y = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# x_squared = x * 2
+# x_3 = x * 3
+# x_input = x * a
